2e3525a601ea0736b9dd58ec2055f08e16c59dde
jagdeeshdembla <107914764+jagdeeshdembla@users.noreply.github.com>
2024-11-11 21:52:57 +0500

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,405 @@
+﻿namespace SIBAU_liblink
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            panel1 = new Panel();
+            pictureBox9 = new PictureBox();
+            About_btn = new Button();
+            pictureBox8 = new PictureBox();
+            Logout_btn = new Button();
+            pictureBox2 = new PictureBox();
+            Reports_btn = new Button();
+            pictureBox7 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            Lists_btn = new Button();
+            Serials_btn = new Button();
+            pictureBox5 = new PictureBox();
+            Cataloging_btn = new Button();
+            pictureBox3 = new PictureBox();
+            button1 = new Button();
+            label2 = new Label();
+            panel2 = new Panel();
+            label1 = new Label();
+            Circulation_btn = new Button();
+            pictureBox4 = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Controls.Add(pictureBox4);
+            panel1.Controls.Add(Circulation_btn);
+            panel1.Controls.Add(pictureBox9);
+            panel1.Controls.Add(About_btn);
+            panel1.Controls.Add(pictureBox8);
+            panel1.Controls.Add(Logout_btn);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(Reports_btn);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(Lists_btn);
+            panel1.Controls.Add(Serials_btn);
+            panel1.Controls.Add(pictureBox5);
+            panel1.Controls.Add(Cataloging_btn);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Dock = DockStyle.Left;
+            panel1.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(171, 555);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.BackColor = Color.White;
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(5, 389);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(30, 23);
+            pictureBox9.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox9.TabIndex = 19;
+            pictureBox9.TabStop = false;
+            // 
+            // About_btn
+            // 
+            About_btn.BackColor = Color.Transparent;
+            About_btn.Cursor = Cursors.Hand;
+            About_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            About_btn.ForeColor = SystemColors.ActiveCaptionText;
+            About_btn.Location = new Point(0, 384);
+            About_btn.Name = "About_btn";
+            About_btn.Size = new Size(171, 33);
+            About_btn.TabIndex = 18;
+            About_btn.Text = "About";
+            About_btn.UseVisualStyleBackColor = false;
+            About_btn.MouseEnter += About_btn_MouseEnter;
+            About_btn.MouseLeave += About_btn_MouseLeave;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.White;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(5, 525);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(30, 23);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 17;
+            pictureBox8.TabStop = false;
+            // 
+            // Logout_btn
+            // 
+            Logout_btn.BackColor = Color.Transparent;
+            Logout_btn.Cursor = Cursors.Hand;
+            Logout_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Logout_btn.ForeColor = SystemColors.ActiveCaptionText;
+            Logout_btn.Location = new Point(0, 520);
+            Logout_btn.Name = "Logout_btn";
+            Logout_btn.Size = new Size(171, 33);
+            Logout_btn.TabIndex = 16;
+            Logout_btn.Text = "LOGOUT";
+            Logout_btn.UseVisualStyleBackColor = false;
+            Logout_btn.Click += Logout_btn_Click;
+            Logout_btn.MouseEnter += Logout_btn_MouseEnter;
+            Logout_btn.MouseLeave += Logout_btn_MouseLeave;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.White;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(6, 349);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(30, 23);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 15;
+            pictureBox2.TabStop = false;
+            // 
+            // Reports_btn
+            // 
+            Reports_btn.BackColor = Color.Transparent;
+            Reports_btn.Cursor = Cursors.Hand;
+            Reports_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Reports_btn.ForeColor = SystemColors.ActiveCaptionText;
+            Reports_btn.Location = new Point(0, 345);
+            Reports_btn.Name = "Reports_btn";
+            Reports_btn.Size = new Size(171, 33);
+            Reports_btn.TabIndex = 14;
+            Reports_btn.Text = "Reports";
+            Reports_btn.UseVisualStyleBackColor = false;
+            Reports_btn.MouseEnter += Reports_btn_MouseEnter;
+            Reports_btn.MouseLeave += Reports_btn_MouseLeave;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.White;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(6, 309);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(30, 23);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 11;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(171, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.White;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(5, 270);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(30, 23);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 13;
+            pictureBox6.TabStop = false;
+            // 
+            // Lists_btn
+            // 
+            Lists_btn.BackColor = Color.Transparent;
+            Lists_btn.Cursor = Cursors.Hand;
+            Lists_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Lists_btn.ForeColor = SystemColors.ActiveCaptionText;
+            Lists_btn.ImageAlign = ContentAlignment.MiddleLeft;
+            Lists_btn.Location = new Point(0, 304);
+            Lists_btn.Name = "Lists_btn";
+            Lists_btn.Size = new Size(171, 33);
+            Lists_btn.TabIndex = 12;
+            Lists_btn.Text = "Lists";
+            Lists_btn.UseVisualStyleBackColor = false;
+            Lists_btn.MouseEnter += Lists_btn_MouseEnter;
+            Lists_btn.MouseLeave += Lists_btn_MouseLeave;
+            // 
+            // Serials_btn
+            // 
+            Serials_btn.BackColor = Color.Transparent;
+            Serials_btn.Cursor = Cursors.Hand;
+            Serials_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Serials_btn.ForeColor = SystemColors.ActiveCaptionText;
+            Serials_btn.ImageAlign = ContentAlignment.MiddleLeft;
+            Serials_btn.Location = new Point(0, 265);
+            Serials_btn.Name = "Serials_btn";
+            Serials_btn.Size = new Size(171, 33);
+            Serials_btn.TabIndex = 11;
+            Serials_btn.Text = "Serials";
+            Serials_btn.UseVisualStyleBackColor = false;
+            Serials_btn.MouseEnter += Serials_btn_MouseEnter;
+            Serials_btn.MouseLeave += Serials_btn_MouseLeave;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.White;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(5, 231);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(30, 23);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 10;
+            pictureBox5.TabStop = false;
+            // 
+            // Cataloging_btn
+            // 
+            Cataloging_btn.BackColor = Color.Transparent;
+            Cataloging_btn.Cursor = Cursors.Hand;
+            Cataloging_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Cataloging_btn.ForeColor = SystemColors.ActiveCaptionText;
+            Cataloging_btn.Location = new Point(0, 226);
+            Cataloging_btn.Name = "Cataloging_btn";
+            Cataloging_btn.Size = new Size(171, 33);
+            Cataloging_btn.TabIndex = 9;
+            Cataloging_btn.Text = "Cataloging";
+            Cataloging_btn.UseVisualStyleBackColor = false;
+            Cataloging_btn.MouseEnter += Cataloging_btn_MouseEnter;
+            Cataloging_btn.MouseLeave += Cataloging_btn_MouseLeave;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.White;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(5, 150);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(27, 23);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click_1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Cursor = Cursors.Hand;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(0, 145);
+            button1.Name = "button1";
+            button1.Size = new Size(171, 33);
+            button1.TabIndex = 7;
+            button1.Text = "Patrons";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            button1.MouseEnter += button1_MouseEnter;
+            button1.MouseLeave += button1_MouseLeave;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.MidnightBlue;
+            label2.Location = new Point(25, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 30);
+            label2.TabIndex = 2;
+            label2.Text = "DashBoard";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(171, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(747, 45);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.MidnightBlue;
+            label1.Location = new Point(78, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(516, 25);
+            label1.TabIndex = 0;
+            label1.Text = "SUKKUR IBA UNIVERSITY KANDH KOT CAMPUS-LIBLINK";
+            // 
+            // Circulation_btn
+            // 
+            Circulation_btn.BackColor = Color.Transparent;
+            Circulation_btn.Cursor = Cursors.Hand;
+            Circulation_btn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Circulation_btn.ForeColor = SystemColors.ActiveCaptionText;
+            Circulation_btn.ImageAlign = ContentAlignment.MiddleLeft;
+            Circulation_btn.Location = new Point(0, 184);
+            Circulation_btn.Name = "Circulation_btn";
+            Circulation_btn.Size = new Size(171, 33);
+            Circulation_btn.TabIndex = 20;
+            Circulation_btn.Text = "Circulation";
+            Circulation_btn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.White;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(6, 189);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(27, 23);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 21;
+            pictureBox4.TabStop = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(918, 555);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            ForeColor = SystemColors.ButtonHighlight;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            Load += Form2_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Button button1;
+        private Button Cataloging_btn;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox3;
+        private Button Lists_btn;
+        private Button Serials_btn;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox2;
+        private Button Reports_btn;
+        private PictureBox pictureBox8;
+        private Button Logout_btn;
+        private Button About_btn;
+        private PictureBox pictureBox9;
+        private PictureBox pictureBox4;
+        private Button Circulation_btn;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIBAU_liblink
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Logout_btn_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            this.button1.BackColor = Color.Gainsboro;
+            this.pictureBox3.BackColor = Color.Gainsboro;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            this.button1.BackColor = Color.Transparent;
+            this.pictureBox3.BackColor = Color.White;
+        }
+
+        private void Circulation_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.Circulation_btn.BackColor = Color.Gainsboro;
+            this.pictureBox4.BackColor = Color.Gainsboro;
+        }
+
+        private void Circulation_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.Circulation_btn.BackColor = Color.Transparent;
+            this.pictureBox4.BackColor = Color.White;
+        }
+
+        private void Cataloging_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cataloging_btn.BackColor = Color.Gainsboro;
+            this.pictureBox5.BackColor = Color.Gainsboro;
+        }
+
+        private void Cataloging_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cataloging_btn.BackColor = Color.Transparent;
+            this.pictureBox5.BackColor = Color.White;
+        }
+
+        private void Serials_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.Serials_btn.BackColor = Color.Gainsboro;
+            this.pictureBox6.BackColor = Color.Gainsboro;
+        }
+
+        private void Serials_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.Serials_btn.BackColor = Color.Transparent;
+            this.pictureBox6.BackColor = Color.White;
+        }
+
+        private void Lists_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.Lists_btn.BackColor = Color.Gainsboro;
+            this.pictureBox7.BackColor = Color.Gainsboro;
+        }
+
+        private void Lists_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.Lists_btn.BackColor = Color.Transparent;
+            this.pictureBox7.BackColor = Color.White;
+        }
+
+        private void Reports_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.Reports_btn.BackColor = Color.Gainsboro;
+            this.pictureBox2.BackColor = Color.Gainsboro;
+
+        }
+
+        private void Reports_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.Reports_btn.BackColor = Color.Transparent;
+            this.pictureBox2.BackColor = Color.White;
+        }
+
+        private void About_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.About_btn.BackColor = Color.Gainsboro;
+            this.pictureBox9.BackColor = Color.Gainsboro;
+        }
+
+        private void About_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.About_btn.BackColor = Color.Transparent;
+            this.pictureBox9.BackColor = Color.White;
+
+        }
+
+        private void Logout_btn_MouseEnter(object sender, EventArgs e)
+        {
+            this.Logout_btn.BackColor = Color.Gainsboro;
+            this.pictureBox8.BackColor = Color.Gainsboro;   
+
+        }
+
+        private void Logout_btn_MouseLeave(object sender, EventArgs e)
+        {
+            this.Logout_btn.BackColor = Color.Transparent;
+            this.pictureBox8.BackColor= Color.White;
+
+        }
+    }
+}
